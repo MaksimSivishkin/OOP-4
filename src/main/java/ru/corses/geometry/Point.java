cc00f32c5b68d2c371b4eb3239cab9fee1939938
Sivishkin Maksim <MSivishkin127@gmail.com>
2025-03-24 15:39:57 +0300

    ООП 4 Задание 1

--- src/main/java/ru/corses/geometry/Point.java
+++ src/main/java/ru/corses/geometry/Point.java
@@ -1,5 +1,7 @@
 package ru.corses.geometry;
 
+import java.util.Objects;
+
 public class Point implements Cloneable {
     protected int x;
     protected int y;
@@ -29,13 +31,27 @@ public class Point implements Cloneable {
         return String.format("{%s;%s}", x, y);
     }
 
+
     @Override
     public Point clone() {
         try {
             Point clone = (Point) super.clone();
+            // TODO: copy mutable state here, so the clone can't change the internals of the original
             return clone;
         } catch (CloneNotSupportedException e) {
             throw new AssertionError();
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
